53fa5c0f21bbf77a03788e49005266eef24005fb
ipz214_shchss <ipz214_shchss@student.ztu.edu.ua>
2023-12-10 15:39:34 +0200

    Added command part

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    parser: 'babel-eslint',
+    extends: ['react-app', 'plugin:react/recommended'],
+    rules: {
+        "arrow-body-style": ["error", "as-needed"],
+        eqeqeq: 'error',
+        'no-unused-vars': 'error',
+        'no-mixed-spaces-and-tabs': 'error',
+    },
+  };
+  
